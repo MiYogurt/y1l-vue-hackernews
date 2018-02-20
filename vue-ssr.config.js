3f6fe27b233e3ec05bf74bd72f9dd7352af08f88
yugo <belovedyogurt@gmail.com>
2018-02-20 18:19:18 +0800

    remove service worker config

--- vue-ssr.config.js
+++ vue-ssr.config.js
@@ -4,61 +4,66 @@
 //     status404?: string;
 //     status500?: string;
 //     template?: string;
-//     context?: object;
+//     context?: async function;
 //     handleError?: Function;
 //     webpackConfig?: {
 //         base: object;
 //         client: object;
 //         server: object;
-//     };
-//     serviceWorkerConfig?: object;
+//     }
 // }
 //
 
 module.exports.default = module.exports = {
-    projectPath: __dirname,
-    context: {
-        title: "Vue HN 2.0"
+    context: req => {
+        title: 'Vue HN 2.0'
     },
     webpackConfig: {
         client: {
             resolve: {
                 alias: {
-                    "create-api": "./create-api-client.js"
+                    'create-api': './create-api-client.js'
                 }
             }
         },
         server: {
             resolve: {
                 alias: {
-                    "create-api": "./create-api-server.js"
+                    'create-api': './create-api-server.js'
                 }
             }
         }
-    },
-    serviceWorkerConfig: {
-        cacheId: "vue-hn",
-        filename: "service-worker.js",
-        minify: true,
-        dontCacheBustUrlsMatching: /./,
-        staticFileGlobsIgnorePatterns: [/\.map$/, /\.json$/],
-        runtimeCaching: [
-            {
-                urlPattern: "/",
-                handler: "networkFirst"
-            },
-            {
-                urlPattern: /\/(top|new|show|ask|jobs)/,
-                handler: "networkFirst"
-            },
-            {
-                urlPattern: "/item/:id",
-                handler: "networkFirst"
-            },
-            {
-                urlPattern: "/user/:id",
-                handler: "networkFirst"
-            }
-        ]
     }
-};
+}
+
+if (process.env.NODE_ENV === 'production') {
+    module.exports.client.plugins.push(
+        new SWPrecachePlugin({
+            cacheId: 'vue-hn',
+            filename: 'service-worker.js',
+            minify: true,
+            dontCacheBustUrlsMatching: /./,
+            staticFileGlobsIgnorePatterns: [/\.map$/, /\.json$/],
+            runtimeCaching: [
+                {
+                    urlPattern: '/',
+                    handler: 'networkFirst'
+                },
+                {
+                    urlPattern: /\/(top|new|show|ask|jobs)/,
+                    handler: 'networkFirst'
+                },
+                {
+                    urlPattern: '/item/:id',
+                    handler: 'networkFirst'
+                },
+                {
+                    urlPattern: '/user/:id',
+                    handler: 'networkFirst'
+                }
+            ]
+        })
+    )
+}
+
+
